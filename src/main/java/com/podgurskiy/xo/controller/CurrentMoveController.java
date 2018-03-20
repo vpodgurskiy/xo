4df64ad7859ffe3d67cf74ff06dc59135bd24e3d
v.podgurskiy <vpodgurskiy@gmail.com>
2018-03-20 12:45:51 +0300

    created method CurrentMoveController for check test in maven

--- src/main/java/com/podgurskiy/xo/controller/CurrentMoveController.java
+++ src/main/java/com/podgurskiy/xo/controller/CurrentMoveController.java
@@ -1,4 +1,14 @@
 package com.podgurskiy.xo.controller;
 
+import com.podgurskiy.xo.model.Figure;
+
 public class CurrentMoveController {
+
+    public final Figure currentMove() {
+
+        return Figure.O;
+    }
+
+
+
 }
